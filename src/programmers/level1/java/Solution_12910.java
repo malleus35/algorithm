4f578b88e3afb854b3457738e2dfb175d98de435
malleus35 <malleus35@gmail.com>
2021-03-10 17:44:22 +0900

    solve programmers 12910

---
+++ src/programmers/level1/java/Solution_12910.java
@@ -0,0 +1,11 @@
+package programmers.level1.java;
+
+import java.util.*;
+
+public class Solution_12910 {
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = Arrays.stream(arr).filter(e -> e % divisor == 0).sorted().toArray();
+        if(answer.length == 0) answer = new int[]{-1};
+        return answer;
+    }
+}
