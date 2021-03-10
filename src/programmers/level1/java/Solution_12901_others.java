a3c3a975e5c4f413b4870c83d819ec961814aac5
malleus35 <malleus35@gmail.com>
2021-03-10 16:44:02 +0900

    add others solve 12901

---
+++ src/programmers/level1/java/Solution_12901_others.java
@@ -0,0 +1,15 @@
+package programmers.level1.java;
+import java.util.*;
+public class Solution_12901_others {
+    public String getDayName(int month, int day) {
+        Calendar cal = new Calendar.Builder().setCalendarType("iso8601")
+                .setDate(2016, month - 1, day).build();
+        return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, new Locale("ko-KR")).toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        Solution_12901_others test = new Solution_12901_others();
+        int a = 5, b = 24;
+        System.out.println(test.getDayName(a, b));
+    }
+}
